17a4f83c16b43337899594dc233c692d4ae2a38e
corona <p54321ppp-qe9@yahoo.co.jp>
2012-05-04 12:48:18 +0900

    change top page

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-<?php echo "Welcome!"; ?>
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css" />
+		<style>
+			body {
+				padding-top: 3em;
+			}
+		</style>
+		<title>coronet</title>
+	</head>
+	<body>
+		<div class="container">
+			<hr />
+			<div class="row">
+				<div class="span4">
+					<a href="http://twitter.com/corona266" class="btn btn-info">Twitter</a>
+				</div>
+				<div class="span4">
+					<a href="http://yui7.tk/" class="btn btn-info">weblog</a>
+				</div>
+				<div class="span4">
+					<a href="http://b.yui7.tk/" class="btn btn-info">mail</a>
+				</div>
+			</div>
+			<hr />
+			<footer>&copy; corona</footer>
+		</div>
+	</body>
+</html>
+
